52602efca2a7fd39f789d4c3923b2d86ed51df8f
yge <yge0329@gmail.com>
2023-10-09 17:38:55 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,51 @@
-import React from "react"
+import React from "react";
+import about from "../assets/img/about.jpg"; //about에 임포트, 임포트한거 사용할때는 {about} 이렇게 사용
+
+const introText = {
+    title: "port developer",
+    desc: [
+        "talent is",
+        "found at the end of the",
+        "effort"
+    ]
+}
 
 const Intro = () => {
     return (
-        <div>Intro</div>
-    )
-}
+        <section id="intro">
+            <div className="intro__inner">
+                <h1 className="intro__title">{introText.title}</h1>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="intro__text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="어바웃" />
+                    </div>
+                </div>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Intro
+export default Intro;
